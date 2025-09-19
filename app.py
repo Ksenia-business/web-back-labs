8df3518eceaf5828020b3fdc5d860a1fcf1b41fe
Ксения Чепурнова <ksen.chepurnova@yandex.ru>
2025-09-19 21:34:17 +0700

    Имя студента

--- app.py
+++ app.py
@@ -435,4 +435,11 @@ def add_flower(name):
 @app.route('/lab2/example')
 def example():
     name = 'Ксения Чепурнова'
-    return render_template('example.html', name=name)
+    number_lab = 'Лабораторная работа 2'
+    group = 'ФБИ-31'
+    course = '3 курс'
+    return render_template('example.html', 
+                           name=name,
+                           number_lab=number_lab,
+                           group=group,
+                           course=course)
