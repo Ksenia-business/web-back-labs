d96e21121ca9c966fd577fb379a4bb0d2686c543
Ксения Чепурнова <ksen.chepurnova@yandex.ru>
2025-09-28 17:50:47 +0700

    Книги

--- app.py
+++ app.py
@@ -600,3 +600,82 @@ def calculate_operations_any(a, b):
 @app.route('/lab2/calc/<int:a>')
 def calc_with_one_default(a):
     return redirect(f'/lab2/calc/{a}/1')
+
+books = [
+    {
+        'author': 'Фёдор Достоевский',
+        'title': 'Преступление и наказание',
+        'genre': 'Роман',
+        'pages': 672
+    },
+    {
+        'author': 'Лев Толстой',
+        'title': 'Война и мир',
+        'genre': 'Роман-эпопея',
+        'pages': 1220
+    },
+    {
+        'author': 'Михаил Булгаков',
+        'title': 'Мастер и Маргарита',
+        'genre': 'Роман',
+        'pages': 480
+    },
+    {
+        'author': 'Антон Чехов',
+        'title': 'Рассказы',
+        'genre': 'Рассказы',
+        'pages': 320
+    },
+    {
+        'author': 'Александр Пушкин',
+        'title': 'Евгений Онегин',
+        'genre': 'Роман в стихах',
+        'pages': 384
+    },
+    {
+        'author': 'Николай Гоголь',
+        'title': 'Мёртвые души',
+        'genre': 'Поэма',
+        'pages': 352
+    },
+    {
+        'author': 'Иван Тургенев',
+        'title': 'Отцы и дети',
+        'genre': 'Роман',
+        'pages': 288
+    },
+    {
+        'author': 'Александр Грибоедов',
+        'title': 'Горе от ума',
+        'genre': 'Комедия',
+        'pages': 160
+    },
+    {
+        'author': 'Михаил Лермонтов',
+        'title': 'Герой нашего времени',
+        'genre': 'Роман',
+        'pages': 224
+    },
+    {
+        'author': 'Иван Гончаров',
+        'title': 'Обломов',
+        'genre': 'Роман',
+        'pages': 640
+    },
+    {
+        'author': 'Александр Островский',
+        'title': 'Гроза',
+        'genre': 'Драма',
+        'pages': 416
+    },
+    {
+        'author': 'Николай Лесков',
+        'title': 'Левша',
+        'genre': 'Повесть',
+        'pages': 144
+    }
+]
+
+@app.route('/lab2/books')
+def show_books():
+    return render_template('books.html', books=books)
