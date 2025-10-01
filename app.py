1e3dc1e917228c1a400959ff50ad5b10e1ebe9f0
Ксения Чепурнова <ksen.chepurnova@yandex.ru>
2025-10-01 16:27:16 +0700

    Рефакторинг: выделение первой лабораторной

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, url_for, request, redirect, abort, render_template
+from lab1 import lab1
+
 import datetime
 app = Flask(__name__)
+app.register_blueprint(lab1)
 
 @app.route("/")
 @app.route("/index")
@@ -20,58 +23,6 @@ def index():
             <ul>
                 <li><a href="/lab1">Первая лабораторная</a></li>
                 <li><a href="/lab2">Вторая лабораторная</a></li>
-            </ul>
-        </nav>
-        
-        <footer>
-            <hr>
-            <p>&copy Чепурнова Ксения Анатольевна, ФБИ-31, 3 курс, 2025</p>
-        </footer>
-    </body>
-</html>
-'''
-
-@app.route("/lab1")
-def lab1():
-    return '''
-<!doctype html>
-<html>
-    <head>
-        <title>Лабораторная 1</title>
-    </head>
-    <body>
-        <header>
-            <h1>НГТУ, ФБ, WEB-программирование, часть 2. Лабораторная 1</h1>
-        </header>
-        
-        <nav>
-            <h1>Лабораторная работа 1</h1>
-            <p>
-                Flask — фреймворк для создания веб-приложений на языке
-                программирования Python, использующий набор инструментов
-                Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
-                называемых микрофреймворков — минималистичных каркасов
-                веб-приложений, сознательно предоставляющих лишь самые ба
-                зовые возможности.
-            </p>
-            <a href="/">На главную</a>
-
-            <h2>Список роутов</h2>
-            <ul>
-                <li><a href="/lab1/web">/lab1/web</a> - Веб-сервер на Flask</li>
-                <li><a href="/lab1/author">/lab1/author</a> - Информация об авторе</li>
-                <li><a href="/lab1/image">/lab1/image</a> - Изображение дуба</li>
-                <li><a href="/lab1/counter">/lab1/counter</a> - Счетчик посещений</li>
-                <li><a href="/lab1/clean_counter">/lab1/clean_counter</a> - Очистка счетчика</li>
-                <li><a href="/lab1/info">/lab1/info</a> - Перенаправление на автора</li>
-                <li><a href="/lab1/created">/lab1/created</a> - Страница создания (201)</li>
-                <li><a href="/lab1/error">/lab1/error</a> - Вызов ошибки сервера (505)</li>
-                <li><a href="/400">/400</a> - Ошибка 400. Bad Request</li>
-                <li><a href="/401">/401</a> - Ошибка 401. Unauthorized</li>
-                <li><a href="/402">/402</a> - Ошибка 402. Payment Required</li>
-                <li><a href="/403">/403</a> - Ошибка 403. Forbidden</li>
-                <li><a href="/405">/405</a> - Ошибка 405. Method Not Allowed</li>
-                <li><a href="/418">/418</a> - Ошибка 418. I'm a teapot</li>
             </ul>
         </nav>
         
@@ -162,118 +113,6 @@ def not_found(err):
     
     return html_template, 404
 
-@app.route("/lab1/web")
-def web():
-    return """<!doctype html>
-        <html>
-           <body>
-               <h1>web-сервер на flask</h1>
-               <a href="/lab1/author">author</a>
-           </body>
-        </html>""", 200, {
-            'X-Server': 'sample',
-            'Content-Type': 'text/plain; charset=utf-8'
-            }
-
-@app.route("/lab1/author")
-def author():
-    name = "Чепурнова Ксения Анатольевна"
-    group = "ФБИ-31"
-    faculty = "ФБ"
-
-    return """<!doctype html>
-        <html>
-           <body>
-               <p>Студент: """ + name + """</p>
-               <p>Группа: """ + group + """</p>
-               <p>Факультет: """ + faculty + """</p>
-               <a href="/lab1/web">web</a>
-           </body>
-        </html>"""
-
-@app.route("/lab1/image")
-def image():
-    image_path = url_for("static", filename="oak.jpg")
-    css_path = url_for("static", filename="lab1.css")
-    html_content = '''
-<!doctype html>
-<html>
-    <head>
-        <link rel="stylesheet" href="''' + css_path + '''">
-    </head>
-    <body>
-        <div class="container">
-            <h1>Дуб</h1>
-            <img src="''' + image_path + '''" alt="Дуб" class="oak-image">
-            <p class="description">«Сегодняшний огромный дуб — просто вчерашний жёлудь, настоявший на своём» (Дэвид Айк)</p>
-        </div>
-    </body>
-</html>
-'''
-
-    return html_content, 200, {
-            'Content-Language': 'ru',  
-            'X-Image-Type': 'nature',
-            'X-Author-Name': 'Chepurnova Ksenia'
-        }
-
-count = 0
-
-@app.route("/lab1/counter")
-def counter():
-    global count
-    count += 1
-    time = datetime.datetime.today()
-    url = request.url
-    client_ip = request.remote_addr
-
-    return '''
-<!doctype html>
-<html>
-    <body>
-        Сколько раз вы сюда заходили: ''' + str(count) + '''
-        <hr>
-        Дата и время: ''' + str(time) + '''<br>
-        Запрошенный адрес: ''' + url + '''<br>
-        Ваш IP адрес: ''' + client_ip + '''<br>
-        <hr>
-        <a href="/lab1/clean_counter">Очистить счетчик</a>
-    </body>
-</html>
-'''
-
-@app.route("/lab1/clean_counter")
-def clean_counter():
-    global count
-    count = 0
-
-    return '''
-<!doctype html>
-<html>
-    <body>
-        <h2>Счетчик очищен</h2>
-        Текущее значение счетчика: ''' + str(count) + '''
-        <hr>
-        <a href="/lab1/counter">Вернуться к счетчику</a>
-    </body>
-</html>
-'''
-
-@app.route("/lab1/info")
-def info():
-    return redirect("/lab1/author")
-
-@app.route("/lab1/created")
-def created():
-    return '''
-<!doctype html>
-<html>
-    <body>
-        <h1>Создано успешно</h1>
-        <div><i>что-то создано...</i></div>
-    </body>
-</html>
-''', 201
 
 @app.route("/400")
 def bad_request():
@@ -395,11 +234,6 @@ def internal_server_error(err):
 </html>
 ''', 500
 
-@app.route("/lab1/error")
-def cause_error():
-    result = 50 / 0
-    return "Этот код никогда не выполнится"
-
 
 @app.route('/lab2/a')
 def a():
@@ -409,7 +243,6 @@ def a():
 def a2():
     return 'со слэшем'
 
-# Список цветов теперь с ценами
 flower_list = [
     {'name': 'роза', 'price': 150},
     {'name': 'тюльпан', 'price': 80},
