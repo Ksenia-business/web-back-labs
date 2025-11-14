16e7eae9adaf3b3f41bc74f9de875176cf4d4647
Ксения Чепурнова <ksen.chepurnova@yandex.ru>
2025-11-14 21:04:57 +0700

    Заготовка к 6-ой лабораторной

---
+++ lab6.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, request, render_template, redirect, session, current_app
+lab6 = Blueprint('lab6', __name__)
+
+
+@lab6.route('/lab6/')
+def main():
+    return render_template('lab6/lab6.html')
