f23c737e0ada5944813ce7cac5ace10909caf838
Ксения Чепурнова <ksen.chepurnova@yandex.ru>
2025-12-05 12:58:05 +0700

    Редактирование фильмов

--- static/lab7/main.js
+++ static/lab7/main.js
@@ -22,6 +22,9 @@ function fillFilmList() {
             let editButton = document.createElement('button');
             editButton.className = 'btn-edit';
             editButton.innerText = 'редактировать';
+            editButton.onclick = function() {
+                editFilm(i);
+            };
 
             let delButton = document.createElement('button');
             delButton.className = 'btn-delete';
@@ -81,6 +84,7 @@ function addFilm() {
 }
 
 function sendFilm() {
+    const id = document.getElementById('id').value;
     const film = {
         title: document.getElementById('title').value,
         title_ru: document.getElementById('title-ru').value,
@@ -88,8 +92,8 @@ function sendFilm() {
         description: document.getElementById('description').value
     }
 
-    const url = `/lab7/rest-api/films/`;
-    const method = 'POST';
+    const url = `/lab7/rest-api/films/${id}`;
+    const method = id === '' ? 'POST': 'PUT';
 
     fetch(url, {
         method: method,
@@ -99,5 +103,20 @@ function sendFilm() {
     .then(function() {
         fillFilmList();
         hideModal();
+    });
+}
+
+function editFilm(id) {
+    fetch(`/lab7/rest-api/films/${id}`)
+    .then(function (data) {
+        return data.json();
+    })
+    .then(function (film) {
+        document.getElementById('id').value = id;
+        document.getElementById('title').value = film.title;
+        document.getElementById('title-ru').value = film.title_ru;
+        document.getElementById('year').value = film.year;
+        document.getElementById('description').value = film.description;
+        showModal();
     });
 }
